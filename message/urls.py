75457d0d7385a22e969ff4d185d180ab877ac540
zekeriv <ezekielcrivera@gmail.com>
2025-10-20 16:06:57 +1300

    updated chatroom logic

--- message/urls.py
+++ message/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from message.views import send_message, LoginView
+from message.views import send_message, LoginView, LogoutView
 from message.viewsets import Chat_roomViewSet, MessageViewSet
 
 router = DefaultRouter()
@@ -30,5 +30,6 @@ router.register(r'messages', MessageViewSet, basename='message')
 urlpatterns = [
     path('', include(router.urls)),
     path('send_message/', send_message, name='send_message'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
